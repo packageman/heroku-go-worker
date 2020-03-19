91049c715bda7940b19fd7eecab296d8ce740a29
Byron Zhang <byron.zhang@maiscrm.com>
2020-03-19 15:48:35 +0800

    init project

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/heroku/heroku-go-worker
+
+go 1.13
+
+require (
+	github.com/gomodule/redigo v2.0.0+incompatible
+	github.com/heroku/x v0.0.0-20171004170240-705849e307dd
+)
---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/gomodule/redigo/redis"
+	_ "github.com/heroku/x/hmetrics/onload"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+	pool := newRedisPool()
+
+	ticker := time.NewTicker(10 * time.Second)
+	for {
+		select {
+		case <-ticker.C:
+			conn := pool.Get()
+			defer conn.Close()
+			job, err := conn.Do("LPOP", "queue")
+			if err != nil {
+				log.Printf("failed to get job, err: %s", err.Error())
+				continue
+			}
+			if job != "" {
+				log.Printf("got job: %s", job)
+				continue
+			}
+			log.Printf("no job found in queue")
+		}
+	}
+}
+
+func newRedisPool() *redis.Pool {
+	redisUrl := os.Getenv("REDIS_URL")
+	return &redis.Pool{
+		MaxIdle:   5,
+		MaxActive: 12000,
+		Dial: func() (redis.Conn, error) {
+			c, err := redis.DialURL(redisUrl)
+			if err != nil {
+				panic(err.Error())
+			}
+			return c, err
+		},
+	}
+}
